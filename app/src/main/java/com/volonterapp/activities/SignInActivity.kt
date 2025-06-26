6b56cbf293a468bae4bb437d388e56bf7a36e55a
luks <luka1495@gmail.com>
2025-06-26 10:50:19 +0200

    Updated singin, singup and splash activities

--- app/src/main/java/com/volonterapp/activities/SignInActivity.kt
+++ app/src/main/java/com/volonterapp/activities/SignInActivity.kt
@@ -3,6 +3,7 @@ package com.volonterapp.activities
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
@@ -12,20 +13,20 @@ import com.volonterapp.model.User
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : BaseActivity() {
-    /**
-     * This function is auto created by Android when the Activity Class is created.
-     */
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        //This call the parent constructor
         super.onCreate(savedInstanceState)
-        // This is used to align the xml view to this class
         setContentView(R.layout.activity_sign_in)
 
-        // This is used to hide the status bar and make the splash screen as a full screen activity.
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
 
         setupActionBar()
 
@@ -34,9 +35,7 @@ class SignInActivity : BaseActivity() {
         }
     }
 
-    /**
-     * A function for actionBar Setup.
-     */
+
     private fun setupActionBar() {
 
         setSupportActionBar(toolbar_sign_in_activity)
@@ -50,23 +49,17 @@ class SignInActivity : BaseActivity() {
         toolbar_sign_in_activity.setNavigationOnClickListener { onBackPressed() }
     }
 
-    /**
-     * A function for Sign-In using the registered user using the email and password.
-     */
+
     private fun signInRegisteredUser() {
-        // Here we get the text from editText and trim the space
         val email: String = et_email.text.toString().trim { it <= ' ' }
         val password: String = et_password.text.toString().trim { it <= ' ' }
 
         if (validateForm(email, password)) {
-            // Show the progress dialog.
             showProgressDialog(resources.getString(R.string.please_wait))
 
-            // Sign-In using FirebaseAuth
             FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
-                        // Calling the FirestoreClass signInUser function to get the data of user from database.
                         FirestoreClass().loadUserData(this@SignInActivity)
                     } else {
                         Toast.makeText(
@@ -79,9 +72,7 @@ class SignInActivity : BaseActivity() {
         }
     }
 
-    /**
-     * A function to validate the entries of a user.
-     */
+
     private fun validateForm(email: String, password: String): Boolean {
         return if (TextUtils.isEmpty(email)) {
             showErrorSnackBar("Please enter email.")
@@ -94,9 +85,7 @@ class SignInActivity : BaseActivity() {
         }
     }
 
-    /**
-     * A function to get the user details from the firestore database after authentication.
-     */
+
     fun signInSuccess(user: User) {
 
         hideProgressDialog()
--- app/src/main/java/com/volonterapp/activities/SignUpActivity.kt
+++ app/src/main/java/com/volonterapp/activities/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.volonterapp.activities
 
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
@@ -15,32 +16,29 @@ import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : BaseActivity() {
 
-    /**
-     * This function is auto created by Android when the Activity Class is created.
-     */
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        //This call the parent constructor
         super.onCreate(savedInstanceState)
-        // This is used to align the xml view to this class
         setContentView(R.layout.activity_sign_up)
 
-        // This is used to hide the status bar and make the splash screen as a full screen activity.
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
 
         setupActionBar()
 
-        // Click event for sign-up button.
         btn_sign_up.setOnClickListener {
             registerUser()
         }
     }
 
-    /**
-     * A function for actionBar Setup.
-     */
+
     private fun setupActionBar() {
 
         setSupportActionBar(toolbar_sign_up_activity)
@@ -54,36 +52,27 @@ class SignUpActivity : BaseActivity() {
         toolbar_sign_up_activity.setNavigationOnClickListener { onBackPressed() }
     }
 
-    /**
-     * A function to register a user to our app using the Firebase.
-     * For more details visit: https://firebase.google.com/docs/auth/android/custom-auth
-     */
+
     private fun registerUser() {
-        // Here we get the text from editText and trim the space
         val name: String = et_name.text.toString().trim { it <= ' ' }
         val email: String = et_email.text.toString().trim { it <= ' ' }
         val password: String = et_password.text.toString().trim { it <= ' ' }
 
         if (validateForm(name, email, password)) {
-            // Show the progress dialog.
             showProgressDialog(resources.getString(R.string.please_wait))
             FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(
                     OnCompleteListener<AuthResult> { task ->
 
-                        // If the registration is successfully done
                         if (task.isSuccessful) {
 
-                            // Firebase registered user
                             val firebaseUser: FirebaseUser = task.result!!.user!!
-                            // Registered Email
                             val registeredEmail = firebaseUser.email!!
 
                             val user = User(
                                 firebaseUser.uid, name, registeredEmail
                             )
 
-                            // call the registerUser function of FirestoreClass to make an entry in the database.
                             FirestoreClass().registerUser(this@SignUpActivity, user)
                         } else {
                             Toast.makeText(
@@ -96,9 +85,7 @@ class SignUpActivity : BaseActivity() {
         }
     }
 
-    /**
-     * A function to validate the entries of a new user.
-     */
+
     private fun validateForm(name: String, email: String, password: String): Boolean {
         return when {
             TextUtils.isEmpty(name) -> {
@@ -119,9 +106,7 @@ class SignUpActivity : BaseActivity() {
         }
     }
 
-    /**
-     * A function to be called the user is registered successfully and entry is made in the firestore database.
-     */
+
     fun userRegisteredSuccess() {
 
         Toast.makeText(
@@ -130,15 +115,10 @@ class SignUpActivity : BaseActivity() {
             Toast.LENGTH_SHORT
         ).show()
 
-        // Hide the progress dialog
         hideProgressDialog()
 
-        /**
-         * Here the new user registered is automatically signed-in so we just sign-out the user from firebase
-         * and send him to Intro Screen for Sign-In
-         */
+
         FirebaseAuth.getInstance().signOut()
-        // Finish the Sign-Up Screen
         finish()
     }
 }
